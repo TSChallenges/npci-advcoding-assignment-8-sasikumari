266a45ae8d0e882062ab07c4fcd4291170439e45
Sasi Mohan <sasikumari.vetrivelu@npci.org.in>
2025-04-18 22:48:58 +0530

    completed

--- src/main/java/com/mystore/app/config/MyGlobalExceptionHandler.java
+++ src/main/java/com/mystore/app/config/MyGlobalExceptionHandler.java
@@ -15,6 +15,12 @@ import java.util.Map;
 @ControllerAdvice
 public class MyGlobalExceptionHandler {
 
-    // TODO
+@ExceptionHandler(MethodArgumentNotValidException.class)
+public ResponseEntity<Map<String, String>> handleValidationErrors(MethodArgumentNotValidException ex) {
+    Map<String, String> errors = new HashMap<>();
+    ex.getBindingResult().getFieldErrors().forEach(err ->
+        errors.put(err.getField(), err.getDefaultMessage()));
+    return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+}
 
 }
